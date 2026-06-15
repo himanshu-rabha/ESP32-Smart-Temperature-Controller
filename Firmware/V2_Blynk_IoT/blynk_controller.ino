be49174e0da616798734c8b502f7fcec820006e9
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-16 03:00:22 +0530

    Add Blynk IoT temperature controller functionality

---
+++ Firmware/V2_Blynk_IoT/blynk_controller.ino
@@ -0,0 +1,113 @@
+
+
+#define BLYNK_TEMPLATE_ID "TMPL3bvTJEkGo"
+#define BLYNK_TEMPLATE_NAME "ESP32 Temperature Controller V2"
+#define BLYNK_AUTH_TOKEN "6nvA2DYOuMa5CkzTqncA_SZrwF5SEcAk"
+
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+char ssid[] = "Nayanjyoti_";
+char pass[] = "password2024";
+
+#define ONE_WIRE_BUS 15
+#define RELAY_PIN 26
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float setPoint = 45.0;
+
+bool heaterState = false;
+bool emergencyStop = false;
+
+BlynkTimer timer;
+
+BLYNK_WRITE(V2)
+{
+  setPoint = param.asFloat();
+}
+
+BLYNK_WRITE(V3)
+{
+  emergencyStop = param.asInt();
+
+  if(emergencyStop)
+  {
+    heaterState = false;
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
+
+void sendTemperature()
+{
+  sensors.requestTemperatures();
+
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC == -127.0)
+  {
+    Serial.println("Sensor Error");
+
+    digitalWrite(RELAY_PIN, LOW);
+
+    Blynk.virtualWrite(V1, 0);
+
+    return;
+  }
+
+  if(tempC > 70)
+  {
+    heaterState = false;
+  }
+
+   if(!emergencyStop)
+  {
+    if(tempC < (setPoint - 2))
+    {
+      heaterState = true;
+    }
+
+    if(tempC > setPoint)
+    {
+      heaterState = false;
+    }
+  }
+
+
+  digitalWrite(RELAY_PIN, heaterState ? HIGH : LOW);
+
+  Blynk.virtualWrite(V0, tempC);
+  Blynk.virtualWrite(V1, heaterState);
+
+  Serial.print("Temp: ");
+  Serial.print(tempC);
+  Serial.print(" Setpoint: ");
+  Serial.print(setPoint);
+  Serial.print(" Heater: ");
+  Serial.println(heaterState ? "ON" : "OFF");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(RELAY_PIN, OUTPUT);
+
+  digitalWrite(RELAY_PIN, LOW);
+
+  sensors.begin();
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  timer.setInterval(1000L, sendTemperature);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
